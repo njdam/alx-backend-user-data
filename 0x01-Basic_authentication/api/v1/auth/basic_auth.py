81ac69c5a53ca85cc87011ab7d2c4ccb9f586d2d
njdam <jeandamngaboyimana@gmail.com>
2024-04-17 15:13:51 +0000

    Basic auth!

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" A file with a class for authenticating management
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ A class inherits from Auth
+    """
+    pass
