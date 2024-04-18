826d16dced93c06800fd0bd73114cb48f844c05c
njdam <jeandamngaboyimana@gmail.com>
2024-04-18 21:12:31 +0000

    Update on Before request!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,12 +55,14 @@ def authenticate_user():
         excluded_paths = [
                 '/api/v1/status/',
                 '/api/v1/unauthorized/',
-                '/api/v1/forbidden/'
+                '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/'
                 ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
+            sess_cookie = auth.session_cookie(request)
             user = auth.current_user(request)
-            if auth_header is None:
+            if auth_header is None and sess_cookie is None:
                 abort(401)
             if user is None:
                 abort(403)
