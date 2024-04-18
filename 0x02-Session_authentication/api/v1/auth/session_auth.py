cb45e9886aa7efcf19ec3d43267df578fa96d487
njdam <jeandamngaboyimana@gmail.com>
2024-04-18 23:27:17 +0000

    Logout!

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -44,8 +44,8 @@ class SessionAuth(Auth):
         """
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
-        if request is None or session_id is None or user_id is None:
+        if (request is None or session_id is None) or user_id is None:
             return False
-        if session_id in self.user_id_for_session_id:
-            del self.user_id_for_session_id[session_id]
+        if session_id in self.user_id_by_session_id:
+            del self.user_id_by_session_id[session_id]
         return True
