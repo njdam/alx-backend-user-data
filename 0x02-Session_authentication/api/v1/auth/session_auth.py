e3bc8a700756f42a60f61b00799ced4aad45fbf2
njdam <jeandamngaboyimana@gmail.com>
2024-04-18 20:01:31 +0000

    A class SessionAuth that inherits from Auth.

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" A file with a class SessionAuth that inherits from Auth.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ A class SessionAuth that inherits from Auth.
+    """
+    pass
