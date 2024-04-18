430a568822d177d2779795102414c5cb85296b44
njdam <jeandamngaboyimana@gmail.com>
2024-04-18 20:25:41 +0000

    Update SessionAuth class!

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,21 @@
 """ A file with a class SessionAuth that inherits from Auth.
 """
 
+from uuid import uuid4
+
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ A class SessionAuth that inherits from Auth.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ An instance method that create session id for a user_id
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
