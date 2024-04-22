84d3efdefeaa05605485b51636a1decf45ceef9f
njdam <jeandamngaboyimana@gmail.com>
2024-04-22 18:12:18 +0000

    User model!

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,7 +12,7 @@ class User(Base):
     """ User SQLAlchemy model for a database table named users
     """
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
