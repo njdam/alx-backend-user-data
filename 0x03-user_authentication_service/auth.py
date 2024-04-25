cbf975409e5cddc32e88584e0fdf72cea7b2acaa
njdam <jeandamngaboyimana@gmail.com>
2024-04-25 20:05:43 +0000

    Register user!

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,10 @@
 """
 
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +15,39 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash_passwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hash_passwd
+
+
+class Auth:
+    """ Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ A registration of a user
+        Args:
+            email (str): Email of the user
+            password (str): Password of the user
+
+        Returns:
+            User: The newly created User object
+
+        Raises:
+            ValueError: If a user with the given email already exists
+        """
+        try:
+            # Check if a user with the given email already exists
+            existing_user = self._db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass  # No user found, continue with registration
+
+        # Hash the password
+        hashed_password = _hash_password(password)
+
+        # Save the user to the database
+        user = self._db.add_user(email, hashed_password)
+
+        return user
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -59,3 +59,23 @@ print('---------Task 4--------')
 from auth import _hash_password
 
 print(_hash_password("Hello Holberton"))
+
+print('---------Task 5--------')
+from auth import Auth
+
+email = 'me@me.com'
+password = 'mySecuredPwd'
+
+auth = Auth()
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
