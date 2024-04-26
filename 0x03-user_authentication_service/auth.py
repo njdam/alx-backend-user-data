30bb30c6a3a4a5d6c7de432bd27ddf521a266d67
njdam <jeandamngaboyimana@gmail.com>
2024-04-26 04:03:37 +0000

    User authentication service!

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,6 +19,12 @@ def _hash_password(password: str) -> bytes:
     return hash_passwd
 
 
+def _generate_uuid() -> str:
+    """Generates a UUID.
+    """
+    return str(uuid4())
+
+
 class Auth:
     """ Auth class to interact with the authentication database.
     """
